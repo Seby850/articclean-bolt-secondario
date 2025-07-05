8007c52ae06e8ed8ed33a34a110bad954c55ea05
sebyudrescu <sebi.udrescu@yahoo.com>
2025-07-05 23:55:17 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-@@ .. @@
- import Servizi from './pages/Servizi';
- import PulizieUffici from './pages/servizi/PulizieUffici';
- import PulizieIndustriali from './pages/servizi/PulizieIndustriali';
- import PuliziePostCantiere from './pages/servizi/PuliziePostCantiere';
- import PulizieVetri from './pages/servizi/PulizieVetri';
-+import RichidiPreventivo from './pages/RichidiPreventivo';
- import Header from './components/Header';
- import Footer from './components/Footer';
- 
-@@ .. @@
-           <Route path="/servizi/pulizie-industriali" element={<PulizieIndustriali />} />
-           <Route path="/servizi/pulizie-post-cantiere" element={<PuliziePostCantiere />} />
-           <Route path="/servizi/pulizie-vetri" element={<PulizieVetri />} />
-+          <Route path="/richiedi-preventivo" element={<RichidiPreventivo />} />
-         </Routes>
-         <Footer />
-       </div>
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Servizi from './pages/Servizi';
+import PulizieUffici from './pages/servizi/PulizieUffici';
+import PulizieIndustriali from './pages/servizi/PulizieIndustriali';
+import PuliziePostCantiere from './pages/servizi/PuliziePostCantiere';
+import PulizieVetri from './pages/servizi/PulizieVetri';
+import RichidiPreventivo from './pages/RichidiPreventivo';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/servizi" element={<Servizi />} />
+          <Route path="/servizi/pulizie-uffici" element={<PulizieUffici />} />
+          <Route path="/servizi/pulizie-industriali" element={<PulizieIndustriali />} />
+          <Route path="/servizi/pulizie-post-cantiere" element={<PuliziePostCantiere />} />
+          <Route path="/servizi/pulizie-vetri" element={<PulizieVetri />} />
+          <Route path="/richiedi-preventivo" element={<RichidiPreventivo />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
