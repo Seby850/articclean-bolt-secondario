efe9ba42d738774dc5256cf0be25d65815ac8809
sebyudrescu <sebi.udrescu@yahoo.com>
2025-07-08 11:15:26 +0200

    Create SanificazioneAmbienti page and add route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import PulizieUffici from './pages/servizi/PulizieUffici';
 import PulizieIndustriali from './pages/servizi/PulizieIndustriali';
 import PuliziePostCantiere from './pages/servizi/PuliziePostCantiere';
 import PulizieVetri from './pages/servizi/PulizieVetri';
+import SanificazioneAmbienti from './pages/servizi/SanificazioneAmbienti';
 import RichidiPreventivo from './pages/RichidiPreventivo';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -20,6 +21,7 @@ function App() {
         <Route path="/servizi/pulizie-industriali" element={<PulizieIndustriali />} />
         <Route path="/servizi/pulizie-post-cantiere" element={<PuliziePostCantiere />} />
         <Route path="/servizi/pulizie-vetri" element={<PulizieVetri />} />
+        <Route path="/servizi/sanificazione-ambienti" element={<SanificazioneAmbienti />} />
         <Route path="/richiedi-preventivo" element={<RichidiPreventivo />} />
       </Routes>
       <Footer />
